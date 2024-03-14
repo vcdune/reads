73665acc20e975303dc8f426a6613041dfbe6238
Abdul Mendahawi <amendahawi@outlook.com>
2024-03-14 00:46:47 -0400

    initial commit

---
+++ reads/src/App.js
@@ -0,0 +1,14 @@
+// Pages
+import Home from "./pages/Home";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;
---
+++ reads/src/components/Header.js
@@ -0,0 +1,14 @@
+export default function Header() {
+  return (
+    <nav>
+      <ul className="nav">
+        <li><a href="">Unicorn Hunt</a></li>
+        <li><a href="">Home</a></li>
+        <li><a href="">Search</a></li>
+        <li><a href="">Twitter</a></li>
+        <li><a href="">Manifesto</a></li>
+        <li><a href="">Log-In</a></li>
+      </ul>
+    </nav>
+  );
+}
