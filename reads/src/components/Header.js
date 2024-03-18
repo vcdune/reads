cd97f6cd8cbf9dbc01b2ed22a826cdea4a1aa1b3
Abdul Mendahawi <amendahawi@outlook.com>
2024-03-17 20:46:51 -0700

    open a new link

--- reads/src/components/Header.js
+++ reads/src/components/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
         <li><a href="/home"><strong>Project Dune</strong></a></li>
         <li><a href="/blog">Articles</a></li>
         {/* <li><a href="">Search</a></li> */}
-        <li><a href="https://dune-terminal.vercel.app/">Terminal</a></li>
+        <li><a href="https://dune-terminal.vercel.app/" target="_blank" rel="noreferrer">Terminal</a></li>
         <li><a href="/community">Community</a></li>
         <li><a href="/">Newsletter</a></li>
         <li><a href="https://twitter.com/abdulmendahawi" target="_blank" rel="noreferrer">Twitter</a></li>
