5ece0761a06712699441699b2d38447ad6ee0673
Abdul Mendahawi <am13948@nyu.edu>
2024-06-17 18:09:13 -0700

    navigation bar logo added

--- reads/src/components/Header.js
+++ reads/src/components/Header.js
@@ -1,39 +1,20 @@
-import React, { useState, useEffect } from "react";
-import firebase from "firebase/compat/app";
-import "firebase/compat/auth";
-
 export default function Header() {
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    const unsubscribe = firebase.auth().onAuthStateChanged((currentUser) => {
-      setUser(currentUser);
-    });
-
-    return () => {
-      unsubscribe();
-    };
-  }, []);
-
   return (
-    <nav className="hover-link">
-      <ul className="header">
-        <div>
-          <li>
-            <a href="/home">
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="/community-blog">Community</a>
-          </li>
-          <li>
-            <a href="/editor">Publish</a>
-          </li>
-          <li>
-            <a href="/profile">Profile</a>
-          </li>
-        </div>
+    <nav className="header">
+      <h5 className="logo">Arete</h5>
+      <ul className="nav-links">
+        <li>
+          <a href="/home">Home</a>
+        </li>
+        <li>
+          <a href="/community-blog">Community</a>
+        </li>
+        <li>
+          <a href="/editor">Publish</a>
+        </li>
+        <li>
+          <a href="/profile">Profile</a>
+        </li>
       </ul>
     </nav>
   );
