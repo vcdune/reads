699ce2b0ac9266d671fe8192ca39b37fcf1b174c
Abdul Mendahawi <am13948@nyu.edu>
2024-06-19 10:03:29 -0700

    updated svg logo

--- reads/src/components/Header.js
+++ reads/src/components/Header.js
@@ -3,13 +3,11 @@ export default function Header() {
     <nav className="header">
       <h5 className="logo">
         <svg
-          fill="black"
-          width="22px"
-          height="22px"
-          viewBox="-2 -2 24 24"
+          width="24px"
+          height="24px"
+          viewBox="0 0 24 24"
           xmlns="http://www.w3.org/2000/svg"
-          preserveAspectRatio="xMinYMin"
-          class="jam jam-write"
+          fill="#000000"
         >
           <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
           <g
@@ -18,7 +16,12 @@ export default function Header() {
             stroke-linejoin="round"
           ></g>
           <g id="SVGRepo_iconCarrier">
-            <path d="M5.72 14.456l1.761-.508 10.603-10.73a.456.456 0 0 0-.003-.64l-.635-.642a.443.443 0 0 0-.632-.003L6.239 12.635l-.52 1.82zM18.703.664l.635.643c.876.887.884 2.318.016 3.196L8.428 15.561l-3.764 1.084a.901.901 0 0 1-1.11-.623.915.915 0 0 1-.002-.506l1.095-3.84L15.544.647a2.215 2.215 0 0 1 3.159.016zM7.184 1.817c.496 0 .898.407.898.909a.903.903 0 0 1-.898.909H3.592c-.992 0-1.796.814-1.796 1.817v10.906c0 1.004.804 1.818 1.796 1.818h10.776c.992 0 1.797-.814 1.797-1.818v-3.635c0-.502.402-.909.898-.909s.898.407.898.91v3.634c0 2.008-1.609 3.636-3.593 3.636H3.592C1.608 19.994 0 18.366 0 16.358V5.452c0-2.007 1.608-3.635 3.592-3.635h3.592z"></path>
+            {" "}
+            <g>
+              {" "}
+              <path fill="none" d="M0 0h24v24H0z"></path>{" "}
+              <path d="M21 2C6 2 4 16 3 22h1.998c.666-3.333 2.333-5.166 5.002-5.5 4-.5 7-4 8-7l-1.5-1 1-1c1-1 2.004-2.5 3.5-5.5z"></path>{" "}
+            </g>{" "}
           </g>
         </svg>
         <span className="logo-text">arete</span>
