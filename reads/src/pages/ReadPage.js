b455562760894b08e372c4c2687be72bc043f6c4
Abdul Mendahawi <am13948@nyu.edu>
2024-06-18 23:36:53 -0700

    minimal bar redesign

--- reads/src/pages/ReadPage.js
+++ reads/src/pages/ReadPage.js
@@ -5,7 +5,6 @@ import "firebase/compat/firestore";
 
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import MinimalHeader from "../components/MinimalHeader";
 
 const DocumentView = () => {
   const { id } = useParams();
@@ -33,18 +32,61 @@ const DocumentView = () => {
 
   return (
     <>
-      <MinimalHeader />
+      {/* <MinimalHeader /> */}
       <div className="page-view">
         <div className="page-container">
           {documentData ? (
             <>
+              <div
+                style={{
+                  textAlign: "center",
+                  backgroundColor: "#e6e5e3",
+                  borderRadius: "8px",
+                  padding: "5px 3px",
+                }}
+              >
+                <a href="/community-blog" className="return-text">
+                  <svg
+                    width="20px"
+                    height="20px"
+                    viewBox="0 0 24 24"
+                    role="img"
+                    xmlns="http://www.w3.org/2000/svg"
+                    aria-labelledby="returnIconTitle"
+                    stroke="#e1764f"
+                    stroke-width="1.2"
+                    stroke-linecap="square"
+                    stroke-linejoin="miter"
+                    fill="none"
+                    color="#000000"
+                  >
+                    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                    <g
+                      id="SVGRepo_tracerCarrier"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    ></g>
+                    <g id="SVGRepo_iconCarrier">
+                      {" "}
+                      <title id="returnIconTitle">Return</title>{" "}
+                      <path d="M19,8 L19,11 C19,12.1045695 18.1045695,13 17,13 L6,13"></path>{" "}
+                      <polyline points="8 16 5 13 8 10"></polyline>{" "}
+                    </g>
+                  </svg>{" "}
+                  Return
+                </a>{" "}
+                <span style={{ color: "#e1764f" }}> · </span>
+                {documentData.documentTitle}
+                <span style={{ color: "#e1764f" }}> // </span>
+                {documentData.author}
+              </div>
               <h1 className="page-title">{documentData.documentTitle}</h1>
-              <p style={{ textAlign: "center" }}>by {documentData.author}</p>
+              {/* <p style={{ textAlign: "center" }}>{documentData.author}</p> */}
               {documentData.imageURL && documentData.imageURL.trim() !== "" && (
                 <img
                   src={documentData.imageURL}
                   alt={documentData.documentTitle}
-                  className="document-image"
+                  className="docuImage"
                 />
               )}
               <div
